23fc6e7d5f3453d20257bcea3f224483c1b40779
Rémi Scandella <remi.scandella@gmail.com>
2021-01-13 14:04:00 +0100

    Remove redundant errors

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
-import { Systeme } from './modules/systeme/Systeme.js';
-import { Notification } from './modules/app/Notification.js';
 import { App } from './modules/app/App.js';
-import './modules/app/custom-scroll-zoom.js';
 import { Voyage } from './modules/app/Voyage.js';
+import './modules/app/custom-scroll-zoom.js';
 
 
 
@@ -11,15 +9,8 @@ window.onload = App.launch();
 // Gère l'historique
 window.addEventListener('popstate', event => {
   const code = event.state.systeme;
-  try {
-    let voy;
-    if (code != null) voy = new Voyage(code, event.state.date);
-    else              voy = new Voyage();
-    voy.go();
-  }
-  
-  catch(error) {
-    new Notification(getString('erreur-systeme-non-atteint'), 'error');
-    throw 'Erreur de génération';
-  }
+  let voy;
+  if (code != null) voy = new Voyage(code, event.state.date);
+  else              voy = new Voyage();
+  voy.go();
 }, false);
